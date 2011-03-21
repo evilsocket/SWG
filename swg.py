ee3dfcf4441fc7fc70491c096218876a23dcad61
evilsocket <evilsocket@gmail.com>
2011-03-21 02:10:55 +0100

    Little fix on .htaccess gzip compression rules

--- swg.py
+++ swg.py
@@ -122,8 +122,6 @@ DirectoryIndex index.html index.htm index.shtml index.php index.php4 index.php3 
 
 <FilesMatch .*\.(""" + config.page_ext + """)>
   RewriteEngine on
-  RewriteCond %{HTTP_USER_AGENT} !MSIE [OR]
-  RewriteCond %{HTTP_USER_AGENT} !Safari
   RewriteCond %{HTTP:Accept-encoding} gzip
   RewriteCond %{REQUEST_FILENAME}.gz -f
   RewriteRule ^(.*)$ $1.gz [L]
@@ -145,8 +143,6 @@ DirectoryIndex index.html index.htm index.shtml index.php index.php4 index.php3 
 
 <FilesMatch .*\.(""" + config.page_ext + """)>
   RewriteEngine on
-  RewriteCond %{HTTP_USER_AGENT} !MSIE [OR]
-  RewriteCond %{HTTP_USER_AGENT} !Safari
   RewriteCond %{HTTP:Accept-encoding} gzip
   RewriteCond %{REQUEST_FILENAME}.gz -f
   RewriteRule ^(.*)$ $1.gz [L]
